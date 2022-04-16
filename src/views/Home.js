58f44b9625d9a2450a3ab7cbba45c6e188c5b86a
Peyas  chandra das <peyas3500@gmail.com>
2022-04-16 13:06:14 +0600

    error handling

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,40 +1,39 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "../Components/BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "My new website",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 1,
-    },
-    {
-      title: "Welcome party!",
-      body: "lorem ipsum...",
-      author: "yoshi",
-      id: 2,
-    },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setisPending] = useState(true);
+  const [error, setError] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
   useEffect(() => {
-    console.log("Use effect run");
-  });
+    console.log("use effect");
+    fetch("http://localhost:8000/blogs12")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch the data form that resource");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setisPending(false);
+        setError(null);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+        setError(error.message);
+        setisPending(false);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <h3 className="text-center">Blog List</h3>
-      <BlogList blogs={blogs} handleDelete={handleDelete} />
+      {error && <div> {error}</div>}
+
+      {isPending && <div className="text-center"> loading ...</div>}
+
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 };
